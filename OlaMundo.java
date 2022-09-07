983cf74b37503f1a586a006ac30d1c3ce9f97875
Wellington Nogueira <neko@brq.com>
2022-09-06 21:19:39 -0300

    adicionando primeiro arquivo

---
+++ OlaMundo.java
@@ -0,0 +1,12 @@
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo !");
+		System.out.println("oi");
+		System.out.println("o meu não apareceu o package na primeira linha");
+		// TODO Auto-generated method stub
+
+	}
+
+}
